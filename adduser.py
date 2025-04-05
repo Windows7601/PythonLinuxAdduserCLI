633099dc4ce58e336e935ebb36a9bb1d6cf052b9
Windows7601 <dursannebis.ekranbir@gmail.com>
2025-04-05 23:20:49 +0300

    Update adduser.py to beta 1.2

--- adduser.py
+++ adduser.py
@@ -1,14 +1,44 @@
-import os
-running = True
-while running:
-    delete = input('Delete or add (d or a)')
-    if delete == 'a':
-        username = input('Username')
-        os.system(f'sudo adduser --allow-all-names {username}')
-    elif delete == 'd':
-        username = input('Username')
-        os.system(f'sudo deluser {username}')
-    if input('New') == 'yes':
-        pass
-    else:
+import os 
+running = True 
+while running: 
+	delete = input('Delete or add or add to group or set group (d or a or g or s, respectively)') 
+    if delete == 'a': 
+        username = input('Username:') 
+        try:
+        		os.system(f'sudo adduser --allow-all-names {username}')
+        except:
+            print('Sorry, an error occured and you need to restart the script.')
+    elif delete == 'd': 
+        username = input('Username:') 
+        sure = input("Are you sure? (y or n):") 
+        if sure == "y": 
+            try:
+            		os.system(f'sudo deluser {username}')
+            except:
+                print('Sorry, an error occured and you need to restart the script.')
+                running = False
+                pass
+    elif delete == 'g':
+        username = input('Username:')
+        group = input('Group to add:')
+        try:
+        		os.system(f'sudo usermod -aG {group} {username}')
+        except:
+            print('Sorry, an error occured and you need to restart the script.')
+            running = False
+            pass
+    elif delete == 's':
+        username = input('Username:')
+        group = input('Group:')
+        sure = ('Are you sure? (y or n) This deletes the user from existing groups!:')
+        if sure == 'y':
+            try:
+            		os.system(f'sudo usermod -G {group} {username}')
+            except:
+                print('Sorry, an error occured and you need to restart the script.')
+                running = False
+                pass
+    if input('New (y/n)') == 'y': 
+        pass 
+    else: 
         running = False
